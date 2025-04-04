8d1d0471016edaa2e596dc32d68150ea22590810
Ian Jane <163165878+ianjnx@users.noreply.github.com>
2025-04-04 18:29:56 +0800

    Add files via upload

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,10 @@
+setTimeout(function () {
+  $('.about-info-inner').typed({
+    strings: [
+      "<span>Hello, I'm Ian Butaslac. </span>I have a passion for technology and enjoy working with web development. Over time, I have developed skills in HTML, CSS, JavaScript, and Flutter, allowing me to build functional and visually appealing projects. I'm always eager to learn new technologies and improve my skills by taking on new challenges. My goal is to create efficient and user-friendly applications while continuously growing as a developer."
+    ],
+    typeSpeed: 7,
+    contentType: 'html'
+  });
+}, 100);
+
